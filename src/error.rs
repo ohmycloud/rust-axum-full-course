a24dfdbcd94805fc6e067a21955553915013acb7
ohmycloud <ohmycloudy@gmail.com>
2025-09-07 11:13:15 +0800

    add uuid and error type

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,7 @@ use axum::{
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, strum_macros::AsRefStr)]
 pub enum Error {
     LoginFail,
     TicketDeleteFailIdNotFound { id: u64 },
@@ -17,6 +17,38 @@ pub enum Error {
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
         println!("->> {:<12} - {self:?}", "INTO_RES");
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNANDLED_CLIENT_ERROR").into_response()
+
+        // Create a placeholder Axum response.
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        // Insert the Error into the response.
+        response.extensions_mut().insert(self);
+        response
     }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            Self::AuthFailNoAuthTokenCookie
+            | Self::AuthFailTokenWrongFormat
+            | Self::AuthFailCtxNotInRequestExt => (StatusCode::FORBIDDEN, ClientError::NO_AUTH),
+            Error::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+            Error::TicketDeleteFailIdNotFound { .. } => {
+                (StatusCode::NOT_FOUND, ClientError::INVALID_PARAMS)
+            }
+
+            _ => (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS),
+        }
+    }
+}
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 #![allow(unused)]
 
 use axum::{
-    Router,
+    Json, Router,
     extract::{Path, Query},
     middleware,
     response::{Html, IntoResponse, Response},
     routing::{get, get_service},
 };
+use serde_json::json;
 use std::net::SocketAddr;
-use ticket::{error::Result, web::mw_auth::mw_ctx_resolver};
+use ticket::{
+    error::{Error, Result},
+    web::mw_auth::mw_ctx_resolver,
+};
+use uuid::Uuid;
 
 use serde::Deserialize;
 use ticket::{model::ModelController, web};
@@ -38,9 +43,29 @@ async fn handler_greet(Path(name): Path<String>) -> impl IntoResponse {
 
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+
+    let uuid = Uuid::new_v4();
+
+    // Get the eventual response error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|e| e.client_status_and_error());
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string()
+                }
+            });
+            println!("    ->> client_error_body: {client_error_body}");
+            (*status_code, Json(client_error_body)).into_response()
+        });
+    println!("    ->> server log line - {uuid} - Error: {service_error:?}");
+
     println!();
 
-    res
+    error_response.unwrap_or(res)
 }
 
 #[derive(Debug, Deserialize)]
